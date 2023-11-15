99adb23f21d8e723d3b289d6f07cdf81c4471689
MohammedKhalell <Mohammed.Y.Khalell@gmail.com>
2023-11-15 23:55:54 +0300

    Project_1

---
+++ main.js
@@ -0,0 +1,249 @@
+let library = {
+  pokemon: [
+    "https://res.cloudinary.com/beumsk/image/upload/v1547980025/memory/Pokemon/Bulbasaur.png",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547980083/memory/Pokemon/Charmander.png",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547980101/memory/Pokemon/Squirtle.png",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547980116/memory/Pokemon/Pikachu.png",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547980129/memory/Pokemon/Mewtwo.png",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547980142/memory/Pokemon/Mew.png",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547980154/memory/Pokemon/Articuno.png",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547980164/memory/Pokemon/Zapdos.png",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547980175/memory/Pokemon/Moltres.png",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547980186/memory/Pokemon/Eevee.png",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547980025/memory/Pokemon/Bulbasaur.png",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547980083/memory/Pokemon/Charmander.png",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547980101/memory/Pokemon/Squirtle.png",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547980116/memory/Pokemon/Pikachu.png",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547980129/memory/Pokemon/Mewtwo.png",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547980142/memory/Pokemon/Mew.png",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547980154/memory/Pokemon/Articuno.png",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547980164/memory/Pokemon/Zapdos.png",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547980175/memory/Pokemon/Moltres.png",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547980186/memory/Pokemon/Eevee.png",
+  ],
+  Animals: [
+    "https://media.4-paws.org/c/0/8/f/c08f6e43f0a19be34c146d59d93058d3058bf3f1/VIER%20PFOTEN_2017-10-20_164-2668x2667-600x600.jpg",
+    "https://media.self.com/photos/57d8b7a446d0cb351c8c5e5b/1:1/w_1600%2Cc_limit/Giraff-lead.jpg",
+    "https://media.4-paws.org/b/e/2/d/be2d88ceb9613ac5066bd11dd950faaf2671bef7/VIER%20PFOTEN_2019-03-15_001-1998x1999-600x600.jpg",
+    "https://static.toiimg.com/thumb.cms?msid=82830924&height=600&width=600",
+    "https://www.rosemariepethospital.com/files/stockton_vet/home-feature-2.jpg",
+    "https://zooknoxville.org/wp-content/uploads/2016/02/MG_3215-600x600.jpg",
+    "https://thezoosociety.org/wp-content/uploads/2021/04/Meerkat-12.4-600x600-1.png",
+    "https://www.flaquarium.org/wp-content/uploads/2023/03/african-penguin-600x600-2.jpg",
+    "https://www.wildsheffield.com/wp-content/uploads/2018/06/red-fox-clive-nichols-6-600x600.jpg",
+    "https://www.telegraph.co.uk/content/dam/news/2017/10/26/TELEMMGLPICT000144715823_trans_NvBQzQNjv4BqqVzuuqpFlyLIwiB6NTmJwfSVWeZ_vEN7c6bHu2jJnT8.jpeg?imwidth=680",
+    "https://media.4-paws.org/c/0/8/f/c08f6e43f0a19be34c146d59d93058d3058bf3f1/VIER%20PFOTEN_2017-10-20_164-2668x2667-600x600.jpg",
+    "https://media.self.com/photos/57d8b7a446d0cb351c8c5e5b/1:1/w_1600%2Cc_limit/Giraff-lead.jpg",
+    "https://media.4-paws.org/b/e/2/d/be2d88ceb9613ac5066bd11dd950faaf2671bef7/VIER%20PFOTEN_2019-03-15_001-1998x1999-600x600.jpg",
+    "https://static.toiimg.com/thumb.cms?msid=82830924&height=600&width=600",
+    "https://www.rosemariepethospital.com/files/stockton_vet/home-feature-2.jpg",
+    "https://zooknoxville.org/wp-content/uploads/2016/02/MG_3215-600x600.jpg",
+    "https://thezoosociety.org/wp-content/uploads/2021/04/Meerkat-12.4-600x600-1.png",
+    "https://www.flaquarium.org/wp-content/uploads/2023/03/african-penguin-600x600-2.jpg",
+    "https://www.wildsheffield.com/wp-content/uploads/2018/06/red-fox-clive-nichols-6-600x600.jpg",
+    "https://www.telegraph.co.uk/content/dam/news/2017/10/26/TELEMMGLPICT000144715823_trans_NvBQzQNjv4BqqVzuuqpFlyLIwiB6NTmJwfSVWeZ_vEN7c6bHu2jJnT8.jpeg?imwidth=680",
+  ],
+
+  disney: [
+    "https://res.cloudinary.com/beumsk/image/upload/v1547982044/memory/disney/mickey.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547982088/memory/disney/mowgli.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547982610/memory/disney/tarzan.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547982620/memory/disney/simba.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547982628/memory/disney/aladin.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547982636/memory/disney/blanche%20neige.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547982644/memory/disney/alice.png",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547982653/memory/disney/peter%20pan.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547982663/memory/disney/pinocchio.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547982738/memory/disney/raiponce.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547982044/memory/disney/mickey.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547982088/memory/disney/mowgli.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547982610/memory/disney/tarzan.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547982620/memory/disney/simba.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547982628/memory/disney/aladin.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547982636/memory/disney/blanche%20neige.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547982644/memory/disney/alice.png",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547982653/memory/disney/peter%20pan.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547982663/memory/disney/pinocchio.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547982738/memory/disney/raiponce.jpg",
+  ],
+  pixar: [
+    "https://res.cloudinary.com/beumsk/image/upload/v1547982971/memory/pixar/up.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547982987/memory/pixar/buzz.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547983000/memory/pixar/woody.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547983016/memory/pixar/Remy.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547983032/memory/pixar/Mike.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547983077/memory/pixar/Nemo.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547983114/memory/pixar/wall-e.png",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547983169/memory/pixar/Mr-Incredible.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547983381/memory/pixar/sully.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547983403/memory/pixar/flash%20mcqueen.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547982971/memory/pixar/up.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547982987/memory/pixar/buzz.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547983000/memory/pixar/woody.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547983016/memory/pixar/Remy.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547983032/memory/pixar/Mike.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547983077/memory/pixar/Nemo.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547983114/memory/pixar/wall-e.png",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547983169/memory/pixar/Mr-Incredible.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547983381/memory/pixar/sully.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547983403/memory/pixar/flash%20mcqueen.jpg",
+  ],
+};
+
+let images = [];
+let tempElt1 = "";
+let tempElt2 = "";
+let click = -1;
+let win = 0;
+let score = 0;
+let time = 0;
+
+let preElt = document.querySelector("#pre");
+let themesElt = document.querySelector("#themes");
+let boxElts = document.getElementsByClassName("box");
+let mainElt = document.querySelector(".main");
+let timeElt = document.querySelector("#time");
+let scoreElt = document.querySelector("#score");
+let postElt = document.querySelector("#post");
+let finalElt = document.querySelector("#final");
+let againElt = document.querySelector("#again");
+let timer = null;
+
+const main1 = document.querySelector("#main1");
+const main2 = document.querySelector("#main2");
+
+function start() {
+  main1.style.display = "none";
+  main2.style.display = "";
+}
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function back() {
+  main2.style.display = "none";
+  main1.style.display = "";
+  resetGame();
+  clearInterval(timer);
+}
+// initiate the game with chosen theme
+themesElt.addEventListener("click", function (e) {
+  if (e.target.classList.contains("themes")) {
+    activateTheme(e.target.id);
+    preElt.classList.add("hidden");
+    show();
+    sleep(10000).then(() => {
+      hide();
+    });
+  }
+});
+
+function activateTheme(theme) {
+  // insert theme in images array
+  for (let i = 0; i < 20; i++) {
+    images.push(library[theme][i]);
+  }
+  // insert images in memory game
+  for (let i = 0; i < 20; i++) {
+    let rand = Math.floor(Math.random() * (images.length - 1));
+    boxElts[i].innerHTML =
+      "<img src='" + images[rand] + "' alt='image' class='hidden'>";
+    images.splice(rand, 1);
+  }
+}
+function show() {
+  for (let i = 0; i < 20; i++) {
+    boxElts[i].firstChild.classList.remove("hidden");
+  }
+}
+function hide() {
+  for (let i = 0; i < 20; i++) {
+    boxElts[i].firstChild.classList.add("hidden");
+  }
+}
+// Handle the play
+mainElt.addEventListener("click", gameLogic);
+
+function gameLogic(e) {
+  // make sure the box is playable
+  if (e.target.classList.contains("play")) {
+    e.target.firstChild.classList.remove("hidden");
+    // first of two click
+    if (click < 1) {
+      tempElt1 = e.target;
+      // timer
+      if (click === -1) {
+        clearInterval(timer);
+        timer = setInterval(function () {
+          time++;
+          timeElt.innerHTML = time;
+        }, 1000);
+      }
+      click = 1;
+    }
+
+    // second click
+    else if (e.target !== tempElt1) {
+      tempElt2 = e.target;
+
+      // different images
+      if (tempElt1.firstChild.src !== tempElt2.firstChild.src) {
+        mainElt.removeEventListener("click", gameLogic);
+        setTimeout(function () {
+          tempElt1.firstChild.classList.add("hidden");
+          tempElt2.firstChild.classList.add("hidden");
+          mainElt.addEventListener("click", gameLogic);
+        }, 400);
+        if (score > 0) {
+          score -= 2;
+        }
+        scoreElt.innerHTML = score;
+      }
+
+      // same images
+      else {
+        score += 10;
+        win += 2;
+        tempElt1.firstChild.classList.add("outlined");
+        tempElt2.firstChild.classList.add("outlined");
+        tempElt1.classList.remove("play");
+        tempElt2.classList.remove("play");
+        scoreElt.innerHTML = score;
+
+        // game won
+        if (win === 20 && score >= 50) {
+          clearInterval(timer);
+          finalElt.innerHTML =
+            "You won <br>" + score + " points <br> in " + time + " seconds";
+          postElt.classList.remove("hidden");
+        } else if (win === 20 && score <= 50) {
+          clearInterval(timer);
+          finalElt.innerHTML =
+            "You lost <br>" + score + " points <br> in " + time + " seconds";
+          postElt.classList.remove("hidden");
+        }
+      }
+      click = 0;
+    }
+  }
+}
+
+againElt.addEventListener("click", resetGame);
+
+function resetGame() {
+  // reset game
+  tempElt1 = "";
+  tempElt2 = "";
+  click = -1;
+  win = 0;
+  score = 0;
+  time = 0;
+  postElt.classList.add("hidden");
+  preElt.classList.remove("hidden");
+  for (let i = 0; i < 20; i++) {
+    boxElts[i].classList.add("play");
+    boxElts[i].firstChild.classList.add("hidden");
+  }
+  timeElt.textContent = time;
+  scoreElt.textContent = score;
+}
